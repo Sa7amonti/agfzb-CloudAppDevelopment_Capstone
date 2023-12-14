d3e6ca39d2ac2fc08f72e4c693df6b4318a7eabb
Sa7amonti <islamgabi@hotmail.com>
2023-12-14 14:41:18 -0500

    dding changes to admin.py and models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -24,12 +24,12 @@ BUILD_CHOICES = (
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 class CarMake(models.Model):
-    make_name = models.CharField(null=False, max_length=30)
-    car_description = models.CharField(null=False, max_length=30)
-    make_year = models.PositiveIntegerField(default=current_year(), validators=[MinValueValidator(1927), max_value_current_year])
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=30)
+    year = models.PositiveIntegerField(default=current_year(), validators=[MinValueValidator(1927), max_value_current_year])
 
     def __str__(self):
-        return self.make_name + " (" + self.make_year + ") : " + self.car_description 
+        return self.name + " (" + str(self.year) + ") : " + self.description 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -42,12 +42,12 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     CarMakes = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
     dealer_id = models.IntegerField()
-    dealer_name = models.CharField(null=False, max_length=30)
-    car_type = models.CharField(max_length=32, choices=BUILD_CHOICES, default='4')
-    make_year = models.DateField(null=True)
+    name = models.CharField(null=False, max_length=30)
+    type = models.CharField(max_length=32, choices=BUILD_CHOICES, default='4')
+    year = models.PositiveIntegerField(default=current_year(), validators=[MinValueValidator(1927), max_value_current_year])
 
     def __str__(self):
-        return self.dealer_name + " : " + self.car_type + ", " + self.make_year
+        return self.name + " : " + self.type + ", " + str(self.year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
